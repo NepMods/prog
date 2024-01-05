aee4e4d0fcd8eaa4cdd60b52a8617175bb7aaecd
nepmods <nalayakoff@gmail.com>
2024-01-05 17:49:02 +0545

    Change: Now can translate Functions

--- Visitor.cpp
+++ Visitor.cpp
@@ -6,40 +6,25 @@ Visitor::Visitor(const nlohmann::json& mainCompound) : currentStackOffset(0) {
     text = "section .text\n";
     code = "";
 
-    text += "global _start";
+    text += "global _start\n";
     code += "_start:\n"\
             "   push    rbp\n"\
             "   mov     rbp, rsp\n"\
             "   sub     rsp, 48\n"\
-            "   mov     dword [rbp - 4], 0\n";
-    generateAssembly(mainCompound, outputFile);
-    code += "   mov eax, 60\n" \
+            "   mov     dword [rbp - 4], 0\n"\
+            "   call _main;\n"\
+            "   mov eax, 60\n" \
             "   xor edi, edi\n"\
             "   syscall\n";
+
+    visit(mainCompound);
     emit(outputFile, data);
     emit(outputFile, text);
     emit(outputFile, code);
     outputFile.close();
 }
 
-long Visitor::generateAssembly(const nlohmann::json& node, std::ostream& output) {
-    const std::string type = node["name"].get<std::string>();
 
-    if (type == "AST_COMPOUND") {
-        for (const auto& child : node["data"]) {
-            generateAssembly(child, output);
-        }
-    } else if (type == "VAR_ADD") {
-        generateVariableDeclaration(node);
-    } else if (type == "VAR_GET") {
-        return generateVariableAccess(node);
-    } else if (type == "FUN_CALL") {
-        generateFunctionCall(node);
-    } else {
-        // Handle other node types if necessary
-    }
-    return 0;
-}
 
 void Visitor::emit(std::ostream& output, const std::string& instruction, const std::string& operand) const {
     output << "" << instruction;
@@ -49,7 +34,45 @@ void Visitor::emit(std::ostream& output, const std::string& instruction, const s
     output << "\n";
 }
 
-void Visitor::generateVariableDeclaration(const nlohmann::json& node) {
+long Visitor::visit(const nlohmann::json& node) {
+    const std::string type = node["name"].get<std::string>();
+
+    if (type == "AST_COMPOUND") {
+        visit_compound(node);
+    }
+    if(type == "AST_FUNCDEF") {
+        visit_function_defincation(node);
+    }
+    if (type == "VAR_ADD") {
+        visit_variable_defination(node);
+    }
+    if (type == "VAR_GET") {
+        return visit_variable_visit(node);
+    }
+    if(type == "FUN_CALL") {
+        visit_function_call(node);
+    }
+    return 0;
+}
+
+void Visitor::visit_compound(const nlohmann::json& node) {
+    for (const auto& child : node["data"]) {
+        visit(child);
+    }
+}
+
+void Visitor::visit_function_defincation(const nlohmann::json& node) {
+    auto name = node["value"].get<std::string>();
+
+    text += "global _"+name+"\n";
+    code += "_"+name+":\n";
+    for (const auto& child : node["data"]) {
+        visit(child);
+    }
+    code += "   ret\n";
+}
+
+void Visitor::visit_variable_defination(const nlohmann::json& node) {
     const std::string varName = node["data"]["name"].get<std::string>();
 
     std::string type =  node["data"]["type"].get<std::string>();
@@ -57,7 +80,7 @@ void Visitor::generateVariableDeclaration(const nlohmann::json& node) {
     if(type == "String") {
         std::string name = "str" + std::to_string(strCount);
         std::string value =  node["data"]["value"]["data"]["value"].get<std::string>();
-        data += name + " db \"" + value + "\", 0\n";
+        data += name + " db \"" + value + "\", 0xA\n";
         code += "   lea     rax, [rel "+ name+"]\n";
         code += "   mov     qword [rbp - "+std::to_string(stackSize+8)+"], rax\n";
         stackSize += 8;
@@ -72,14 +95,14 @@ void Visitor::generateVariableDeclaration(const nlohmann::json& node) {
     var_size[varName] = static_cast<long>(node["data"]["value"]["data"]["value"].get<std::string>().size());
 }
 
-long Visitor::generateVariableAccess(const nlohmann::json& node) {
+long Visitor::visit_variable_visit(const nlohmann::json& node) {
     const std::string varName = node["data"]["value"].get<std::string>();
     const long offset = variableOffsets[varName];
     code += "   mov     rdi, qword [rbp - "+std::to_string(offset)+"]\n";
     return var_size[varName];
 }
 
-void Visitor::generateFunctionCall(const nlohmann::json& node) {
+void Visitor::visit_function_call(const nlohmann::json& node) {
     const std::string funcName = node["data"]["value"].get<std::string>();
 
     if (funcName == "print") {
@@ -88,20 +111,22 @@ void Visitor::generateFunctionCall(const nlohmann::json& node) {
         auto assembly = "   " + node["data"]["params"][0]["data"]["value"].get<std::string>();
         code += assembly + "\n";
     }else {
-
+        code += "call _" + funcName + "\n";
     }
 }
+
+
 
 void Visitor::generatePrintCall(const nlohmann::json& node) {
     const auto& params = node["data"]["params"];
 
     for (const auto& param : params) {
-        long length = generateAssembly(param, outputFile);
+        long length = visit(param);
         code += "   ;Print \n"\
         "   mov     rsi, rdi\n"\
-        "   mov     rdx, "+std::to_string(length)+"\n"\
+        "   mov     rdx, "+std::to_string(length+1)+"\n"\
         "   mov     rax, 1\n"\
-        "    mov     rdi, 1\n"\
+        "   mov     rdi, 1\n"\
         "   syscall\n";
     }
 
