f97adced8c7d5fb82dbe3953a477ef955e7f083a
nepmods <nalayakoff@gmail.com>
2024-01-05 21:29:05 +0545

    Change: Now Compile and run autometically to make testing fast

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include "Lexer.h"
 #include "Visitor.h"
-
+#include <cstdlib>  // for the system function
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <input_file>\n";
@@ -21,6 +21,8 @@ int main(int argc, char* argv[]) {
     Tokenizer tokenizer(source);
     Lexer lexter(tokenizer);
     Visitor visitor(lexter.getCompund());
+
+    std::system("nasm -f elf64 -o output.o output.asm; ld -o output output.o; ./output");
 
     return 0;
 }
